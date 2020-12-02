bfb91ce376b3f99c7944d72e110100f0c61d36b6
letsgetup <sheteswapnil@outlook.com>
2020-12-02 20:04:05 +0530

    ui changess : from UI branch 2-12-2020

--- src/app/shared/components/dash-sidebar/dash-sidebar.component.ts
+++ src/app/shared/components/dash-sidebar/dash-sidebar.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 declare const $: any;
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: 'welcome', title: 'Welcome',  icon:'person', class: '' },
-    { path: 'insureance', title: 'Insureance',  icon:'library_books', class: '' },
-    { path: 'leads', title: 'My Leads',  icon:'content_paste', class: '' },
-    { path: 'booking', title: 'Booking',  icon:'bubble_chart', class: '' },
-    { path: 'profile-view', title: 'User Profile',  icon:'person', class: '' }
+  { path: 'welcome', title: 'Welcome', icon: 'far fa-handshake', class: 'far fa-handshake' },
+  { path: 'insureance', title: 'Insurance', icon: 'library_books', class: 'fas fa-umbrella' },
+  { path: 'leads', title: 'My Leads', icon: 'content_paste', class: 'fas fa-user-friends' },
+  { path: 'booking', title: 'Booking', icon: 'bubble_chart', class: 'fas fa-clipboard-list' },
+  { path: 'profile-view', title: 'User Profile', icon: 'person', class: 'fas fa-user' }
 ];
 
 
